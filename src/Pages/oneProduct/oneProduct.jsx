ffe6c3465d682d9f05742c62feb7c0d61fc779e9
moshe-kur <nuhah3@gmail.com>
2023-01-03 11:53:59 +0200

    change one product page

--- src/Pages/oneProduct/oneProduct.jsx
+++ src/Pages/oneProduct/oneProduct.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import { getOneProductFromDB } from "../../Services";
+import "./style.css";
 
 export const OneProduct = () => {
   let { id } = useParams();
@@ -17,52 +18,37 @@ export const OneProduct = () => {
   }, []);
 
   return (
-    <div>
-      <table>
-        <thead>
-          <tr>
-            <th>ID</th>
-            <th>Name</th>
-            <th>SuplierID</th>
-            <th>CategoryID</th>
-            <th>QuantityPerUnit</th>
-            <th>UnitPrice</th>
-            <th>UnitInStock</th>
-            <th>UnitsOnOrder</th>
-            <th>ReorderLevel</th>
-          </tr>
-        </thead>
-        <tbody>
-          <DataRow product={product}></DataRow>
-        </tbody>
-      </table>
+    <div className="productPage">
+      <ul>
+        <li>ID</li>
+        <li>Name</li>
+        <li>SuplierID</li>
+        <li>CategoryID</li>
+        <li>QuantityPerUnit</li>
+        <li>UnitPrice</li>
+        <li>UnitInStock</li>
+        <li>UnitsOnOrder</li>
+        <li>ReorderLevel</li>
+      </ul>
+
+      <DataRow product={product}></DataRow>
     </div>
   );
 };
 const DataRow = ({ product }) => {
-  let {
-    ID,
-    Name,
-    SuplierID,
-    CategoryID,
-    QuantityPerUnit,
-    UnitPrice,
-    UnitInStock,
-    UnitsOnOrder,
-    ReorderLevel,
-  } = product;
-
   return (
-    <tr>
-      <td>{ID}</td>
-      <td>{Name}</td>
-      <td>{SuplierID}</td>
-      <td>{CategoryID}</td>
-      <td>{QuantityPerUnit}</td>
-      <td>{UnitPrice}</td>
-      <td>{UnitInStock}</td>
-      <td>{UnitsOnOrder}</td>
-      <td>{ReorderLevel}</td>
-    </tr>
+    <ul>
+      <li>{product.ID}</li>
+
+      <input type="text" id="fname" name="fname" value={product.Name}></input>
+
+      <li>{product.SuplierID}</li>
+      <li>{product.CategoryID}</li>
+      <li>{product.QuantityPerUnit}</li>
+      <li>{product.UnitPrice}</li>
+      <li>{product.UnitInStock}</li>
+      <li>{product.UnitsOnOrder}</li>
+      <li>{product.ReorderLevel}</li>
+    </ul>
   );
 };
